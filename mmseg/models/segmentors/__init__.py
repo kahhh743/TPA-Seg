b5c65ed0d315898d5e0936e821a605577e5c6a3d
kahhh743 <1918417202@qq.com>
2025-06-14 13:02:58 +0800

    Update __init__.py

--- mmseg/models/segmentors/__init__.py
+++ mmseg/models/segmentors/__init__.py
@@ -2,10 +2,10 @@
 from .base import BaseSegmentor
 from .cascade_encoder_decoder import CascadeEncoderDecoder
 from .encoder_decoder import EncoderDecoder
-from .seg_tta import SegTTAModel
+
 
 from .aut_encoder_decoder import AUTEncoderDecoder
 
 __all__ = [
-    'BaseSegmentor', 'EncoderDecoder', 'CascadeEncoderDecoder', 'SegTTAModel',  'AUTEncoderDecoder'
+    'BaseSegmentor', 'EncoderDecoder', 'CascadeEncoderDecoder', 'AUTEncoderDecoder'
 ]
